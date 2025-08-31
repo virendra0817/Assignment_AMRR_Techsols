02edcc823ffd21138b3c84fa97269ff00083b62c
Virendra Vikram Singh <153907622+virendra0817@users.noreply.github.com>
2025-08-31 20:16:50 +0530

    Update main.py

--- myapp/main.py
+++ myapp/main.py
@@ -51,4 +51,4 @@ def fetch_transactions(user_id: int, session: Session = Depends(get_session)):
 if __name__ == "__main__":
     
     port = int(os.environ.get("PORT" , 8000))
-   uvicorn.run (myapp.main :app , host ="0.0.0.0",port = port )
+    uvicorn.run (myapp.main :app , host ="0.0.0.0",port = port )
